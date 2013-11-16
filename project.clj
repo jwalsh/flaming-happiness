0cf1e0aaf26905201ca09c5916fe7c3d810032ee
Jason Walsh <jwalsh@bluekai.com>
2013-11-16 09:36:18 -0500

    Correcting and supporting dependency updates
    lein ancient

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1896"]
-                 [ring "1.1.8"]]
+                 [ring "1.1.8"]
+                 [propaganda "0.2.0"]]
   :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.3"]
-            [propaganda "0.2.0"]]
+            [lein-ancient "0.5.4"]]
   :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
   :cljsbuild { 
